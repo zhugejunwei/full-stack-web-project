5b1f55ea86753150f9961ce74413152c4abdf936
zhugejunwei <zhugerock@gmail.com>
2016-02-16 03:10:10 -0500

    /* jshint ignore:end */

--- src/main/resources/public/js/subscribe.js
+++ src/main/resources/public/js/subscribe.js
@@ -116,8 +116,10 @@ $(document).ready(function(){
 });
 
 // last 1
+/* jshint ignore:start */
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+/* jshint ignore:end */
 // last 2
 try {
 var _gaq = _gaq || [];
