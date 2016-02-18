6a18a082dcf241e015240e2f3b074ede1b39e5d0
zhugejunwei <zhugerock@gmail.com>
2016-02-18 18:33:05 -0500

    mainjava

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,12 +34,12 @@ public class Main {
     //   return "E=mc^2: " + energy + " = " + m.toString();
     // });
 
-    // get("/", (request, response) -> {
-    //         Map<String, Object> attributes = new HashMap<>();
-    //         attributes.put("message", "Hello World!");
-    //
-    //         return new ModelAndView(attributes, "index.ftl");
-    //     }, new FreeMarkerEngine());
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "homepage.html");
+        }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
