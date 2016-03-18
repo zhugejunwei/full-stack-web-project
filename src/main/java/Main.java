f05e68c2606c3cd3d8a6e53278aa0f0da8e51409
zhugejunwei <zhugerock@gmail.com>
2016-03-18 18:40:39 -0400

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,37 +45,37 @@ public class Main {
     //         return new ModelAndView(attributes, "index.ftl");
     //     }, new FreeMarkerEngine());
 
-    Object r = new JavaGettingStarted();
-    Object r = new Week6Routes();
-    Object r = new Week7Routes();
-    Object r = new Week8Routes();
+    //Object r = new JavaGettingStarted();
+    //Object r = new Week6Routes();
+    //Object r = new Week7Routes();
+    //Object r = new Week8Routes();
 
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
+      Object r = new newUser();
+//    get("/db", (req, res) -> {
+//      Connection connection = null;
+//      Map<String, Object> attributes = new HashMap<>();
+//      try {
+//        connection = DatabaseUrl.extract().getConnection();
+//
+//        Statement stmt = connection.createStatement();
+//        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+//        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+//        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+//
+//        ArrayList<String> output = new ArrayList<String>();
+//        while (rs.next()) {
+//          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+//        }
+//
+//        attributes.put("results", output);
+//        return new ModelAndView(attributes, "db.ftl");
+//      } catch (Exception e) {
+//        attributes.put("message", "There was an error: " + e);
+//        return new ModelAndView(attributes, "error.ftl");
+//      } finally {
+//        if (connection != null) try{connection.close();} catch(SQLException e){}
+//      }
+//    }, new FreeMarkerEngine());
 
   }
 
