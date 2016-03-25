70353e7d9129692c6e0fbe930b1f67fa8f0bcaa2
zhugejunwei <zhugerock@gmail.com>
2016-03-25 11:04:38 -0400

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,53 +3,40 @@
 // import org.jscience.physics.model.RelativisticModel;
 // import org.jscience.physics.amount.Amount;
 //
-import java.sql.*;
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.Arrays;
 
 import java.net.UnknownHostException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import static spark.SparkBase.staticFileLocation;
-import static spark.Spark.*;
-import spark.template.freemarker.FreeMarkerEngine;
-import spark.ModelAndView;
-import static spark.Spark.get;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//
+//import static spark.SparkBase.staticFileLocation;
+//import static spark.Spark.*;
+//import spark.template.freemarker.FreeMarkerEngine;
+//import spark.ModelAndView;
+//import static spark.Spark.get;
 
 import com.mongodb.*;
-
-import com.heroku.sdk.jdbc.DatabaseUrl;
 
 import static spark.Spark.*;
 
 import com.todoapp.*;
+import java.util.List;
 
 public class Main {
 
-  public static void main(String[] args) throws MongoException, UnknownHostException {
-//        MongoClientURI uri = new MongoClientURI(System.getenv("MONGOHQ_URL"));
-//        MongoClient mongoClient = new MongoClient(uri);
-//        
-//        String dbname = uri.getDatabase();
-//        
-//        //mongoClient.setWriteConcern(WriteConcern.JOURNALED);
-//        DB db = mongoClient.getDatabase(dbname);
-//        
-//        db.authenticate(uri.getUsername(), uri.getPassword());
+  public static void main(String[] args) throws MongoException, UnknownHostException, Exception {
+        MongoClientURI uri = new MongoClientURI(System.getenv("mongodb://heroku:pn8dcfWw1gaUwhB0Ic62Vr-HCd38d5aCCckox1Hs_4EFlQbre-7225F_2nywCjrjlrCU78bdszTPwjCdEZZeig@candidate.67.mongolayer.com:10396,candidate.68.mongolayer.com:10385/app45876994"));
+        MongoClient mongoClient = new MongoClient(uri);
         
-       MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
-        //get connected
-        DB db = mongoURI.connectDB();
-        // authenticate
-        // (version 2.7.2) db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
-        MongoCredential credential = MongoCredential.createCredential(mongoURI.getUsername(), mongoURI.getDatabase(), mongoURI.getPassword());
-        MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+        String dbname = uri.getDatabase();
+        
+        //mongoClient.setWriteConcern(WriteConcern.JOURNALED);
+        DB db = mongoClient.getDB(dbname);
+
         staticFileLocation("/public");
         new TodoResource(new TodoService(db));
-        
-//        mongoClient.close();
+ 
+        MongoCredential credential = MongoCredential.createCredential(uri.getUsername(),dbname,uri.getPassword());
+        MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
     }
 }
