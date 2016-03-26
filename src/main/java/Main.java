e9ac2d5f72f2cfc88b526fc6b76eed96f10a5866
zhugejunwei <zhugerock@gmail.com>
2016-03-26 15:49:52 -0400

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,29 +24,18 @@ import java.util.List;
 
 public class Main {
 
-  public static void main(String[] args) throws MongoException, UnknownHostException, Exception {
-        MongoClientURI uri = new MongoClientURI("mongodb://heroku_9b1msnk8:qg4gd778v5o71j63bleh4ir0c0@ds015869.mlab.com:15869/heroku_9b1msnk8");
-        MongoClient mongoClient = new MongoClient(uri);
-    //    MongoURI mongoURI = new MongoURI(System.getenv("mongodb://heroku_9b1msnk8:qg4gd778v5o71j63bleh4ir0c0@ds015869.mlab.com:15869/heroku_9b1msnk8"));
-        //get connected
-        String dbname = uri.getDatabase();
-        
-        //mongoClient.setWriteConcern(WriteConcern.JOURNALED);
+    public static void main(String[] args) throws Exception {
+		
+			
+		MongoClientURI uri = new MongoClientURI("mongodb://heroku_9b1msnk8:qg4gd778v5o71j63bleh4ir0c0@ds015869.mlab.com:15869/heroku_9b1msnk8");
+		String dbname = uri.getDatabase();
+		MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
+		MongoClient mongoClient = new MongoClient(uri);
+		
+        mongoClient.setWriteConcern(WriteConcern.SAFE);
         DB db = mongoClient.getDB(dbname);
-    //    port(Integer.valueOf(System.getenv("PORT")));
-
-    //MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
-    //DB db = mongoURI.connectDB();
-    //db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
-
-    //DBCollection coll = db.getCollection("todos");
-    
+        
         staticFileLocation("/public");
-    //    Object r = new Week7Routes();
-         new TodoResource(new TodoService(db));
- 
-        MongoCredential credential = MongoCredential.createCredential(uri.getUsername(),dbname,uri.getPassword());
-//        MongoCredential credential = MongoCredential.createCredential(mongoURI.getUsername(), mongoURI.getDatabase(), mongoURI.getPassword());
-        MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
+        new TodoResource(new TodoService(db));
     }
 }
