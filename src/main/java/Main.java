27b6cb951e97b02b28b6e77394d1faac41000f69
zhugejunwei <zhugerock@gmail.com>
2016-03-26 17:45:46 -0400

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,14 +25,27 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	MongoClientURI uri = new MongoClientURI("mongodb://heroku:pn8dcfWw1gaUwhB0Ic62Vr-HCd38d5aCCckox1Hs_4EFlQbre-7225F_2nywCjrjlrCU78bdszTPwjCdEZZeig@candidate.67.mongolayer.com:10396,candidate.68.mongolayer.com:10385/app45876994");
-	MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
-	MongoClient mongoClient = new MongoClient(uri);
-//	
-//        String dbname = uri.getDatabase();
-//        
-//        mongoClient.setWriteConcern(WriteConcern.SAFE);
-        DB db = mongoClient.getDB(uri.getDatabase());
+//	MongoClientURI uri = new MongoClientURI("mongodb://heroku:pn8dcfWw1gaUwhB0Ic62Vr-HCd38d5aCCckox1Hs_4EFlQbre-7225F_2nywCjrjlrCU78bdszTPwjCdEZZeig@candidate.67.mongolayer.com:10396,candidate.68.mongolayer.com:10385/app45876994");
+//	MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
+//	MongoClient mongoClient = new MongoClient(uri);
+//
+//        DB db = mongoClient.getDB(uri.getDatabase());
+        
+        MongoURI mongoURI = new MongoURI("mongodb://heroku_9b1msnk8:qg4gd778v5o71j63bleh4ir0c0@ds015869.mlab.com:15869/heroku_9b1msnk8");
+    //get connected
+    DB db = mongoURI.connectDB();
+    // authenticate
+    // (version 2.7.2) db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
+    MongoCredential credential = MongoCredential.createCredential(mongoURI.getUsername(), mongoURI.getDatabase(), mongoURI.getPassword());
+    MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+    
+    DBCollection coll = db.getCollection("testCollection");
+    BasicDBObject doc = new BasicDBObject("name", "MongoDB")
+    .append("type", "database")
+    .append("count", 1)
+    .append("info", new BasicDBObject("x", 204).append("y", 103));
+    coll.insert(doc);
+    
         //port(Integer.valueOf(System.getenv("PORT")));  
         staticFileLocation("/public");
 //        new TodoResource(new TodoService(db));
